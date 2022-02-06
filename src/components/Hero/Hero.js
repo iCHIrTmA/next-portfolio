378cdebe68acb1a1ab760fe373da1c9024a6fa32
janroeNabepero <janroeNabepero@nabepero.co.jp>
2022-02-07 07:59:02 +0800

    initial commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello there <br />
+        Please enjoy your stay
+      </SectionTitle>
+      <SectionText>
+        This portfolio website showcases my laravel projects that were built
+        using the TALL stack. This website however showcases my enthusiasm for
+        pleasant web design.
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
